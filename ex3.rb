59e50ac626e478bec848166e298559bec2c5b429
stergiman <stephane.tergiman@gmail.com>
2016-05-18 17:27:58 +0200

    Fini les exercices 1 à 5

---
+++ ex3.rb
@@ -0,0 +1,31 @@
+# allows to know how many chickens are available
+puts "I will now count my chickens:"
+
+# allows to know how many hens there are
+puts "Hens #{25 + 30 / 6}"
+# allows to know how many roosters there are
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# how many eggs are there
+puts "Now I will count the eggs:"
+
+# mode of calculation 
+puts 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+
+#an instance of mode of calculation 
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# other mathematical line 
+puts 3 + 2 < 5 - 7 
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 >= -2}"
+
